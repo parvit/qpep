13db432c8b941cecef7d2348e526332f50c21108
Vittorio Parrella <vittorioparrella+1@gmail.com>
2023-04-18 23:29:39 +0200

    download via go test

--- docker/speedtests/speed_test.go
+++ docker/speedtests/speed_test.go
@@ -2,15 +2,22 @@ package speedtests
 
 import (
 	"flag"
+	"github.com/parvit/qpep/logger"
+	"github.com/parvit/qpep/shared"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+	"net"
+	"net/http"
+	"net/url"
 	"testing"
+	"time"
 )
 
-var testTimeout = flag.Int("test_timeout", 60, "execution timeout seconds")
+var targetURL = flag.String("target_url", "", "url to download")
 var connections = flag.Int("connections_num", 1, "simultaneous tcp connections to make to the server")
 
 func TestSpeedTestsConfigSuite(t *testing.T) {
-	t.Log(*testTimeout)
+	t.Log(*targetURL)
 	t.Log(*connections)
 
 	var q SpeedTestsConfigSuite
@@ -19,4 +26,41 @@ func TestSpeedTestsConfigSuite(t *testing.T) {
 
 type SpeedTestsConfigSuite struct {
 	suite.Suite
+}
+
+func (s *SpeedTestsConfigSuite) TestRun() {
+	shared.GetSystemProxyEnabled()
+
+	client := getClientForAPI(nil)
+	assert.NotNil(s.T(), client)
+	assert.NotNil(s.T(), targetURL)
+
+	_, err := client.Get(*targetURL)
+	assert.Nil(s.T(), err)
+}
+
+func getClientForAPI(localAddr net.Addr) *http.Client {
+	dialer := &net.Dialer{
+		LocalAddr: localAddr,
+		Timeout:   5 * time.Second,
+		KeepAlive: 30 * time.Second,
+		DualStack: true,
+	}
+	return &http.Client{
+		Timeout: 30 * time.Second,
+		Transport: &http.Transport{
+			Proxy: func(*http.Request) (*url.URL, error) {
+				logger.Info("API Proxy: %v %v\n", shared.UsingProxy, shared.ProxyAddress)
+				if shared.UsingProxy {
+					return shared.ProxyAddress, nil
+				}
+				return nil, nil
+			},
+			DialContext:     dialer.DialContext,
+			MaxIdleConns:    1,
+			IdleConnTimeout: 10 * time.Second,
+			//TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		},
+	}
 }
