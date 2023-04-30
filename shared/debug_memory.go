3332aaf749acde50774207eba2f9d5bed7222a22
Vittorio Parrella <vittorioparrella+1@gmail.com>
2023-04-30 16:35:09 +0200

    debug heap test

--- shared/debug_memory.go
+++ shared/debug_memory.go
@@ -9,13 +9,12 @@ import (
 
 func DumpMemoryHeapWatcher() {
 	go func() {
+		home := os.Getenv("HOME")
 		for {
-			t, _ := os.OpenFile("dump_"+time.Now().Format(time.RFC3339Nano)+".prof", os.O_RDWR, 0777)
-			defer func() {
-				t.Sync()
-				t.Close()
-			}()
+			t, _ := os.OpenFile(home+"/dump_"+time.Now().Format(time.RFC3339Nano)+".prof", os.O_RDWR, 0777)
 			pprof.WriteHeapProfile(io.Writer(t))
+			t.Sync()
+			t.Close()
 			<-time.After(10 * time.Second)
 		}
 	}()
